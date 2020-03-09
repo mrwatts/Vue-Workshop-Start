0706153d413ef797698e5343e0768e7d289e24d0
mrwatts <famwatts@gmail.com>
2020-03-09 12:50:14 +0100

    Cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img width="25%" src="./assets/logo.png">
-    <HelloWorld msg="Hello Vue in CodeSandbox!" />
+    <HelloWorld msg="Your component goes here..."/>
   </div>
 </template>
 
@@ -17,12 +17,14 @@ export default {
 </script>
 
 <style>
+body {
+  background-color: #000;
+  color: #FFF;
+}
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
