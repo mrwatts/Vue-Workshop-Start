87750923ed8810bf6ec3f94ac8a2016ea4fd1dd0
mrwatts <famwatts@gmail.com>
2020-03-10 14:19:43 +0100

    updates

--- src/helpers.js
+++ src/helpers.js
@@ -1,18 +1,7 @@
 import moment from "moment";
-import _ from "lodash";
-
-export function getCalendarMonth(year, month, day) {
-  var arr = getItemsPerMonth(year, month, day);
-
-  var result = _.groupBy(arr, function(m) {
-    return m.date.isoWeek();
-  });
-
-  return result;
-}
 
 export function getItemsPerMonth(year, month) {
-  var n = getIsoWeekDaysInMonth(year, month, 1);
+  var n = getIsoWeekDaysInMonth(year, month);
 
   var arr = Array.from(
     {
@@ -24,10 +13,9 @@ export function getItemsPerMonth(year, month) {
         .add(i, "days")
   );
 
-  var reqMonthId = moment(year + "-" + month + "-01", "YYYY-MM-DD").monthId();
+  const reqMonthId = moment(year + "-" + month + "-01", "YYYY-MM-DD").monthId();
   return arr.map(x => {
     return {
-      dayId: x.dayId(),
       date: x,
       labelCount: 0,
       isInReqMonth: reqMonthId === x.monthId()
@@ -35,8 +23,8 @@ export function getItemsPerMonth(year, month) {
   });
 }
 
-export function getFullMonthName(year, month) {
-  return moment(year + "-" + month + "-01", "YYYY-MM-DD").format("MMMM");
+export function getFullMonthName(month) {
+  return moment("2020-" + month + "-01", "YYYY-MM-DD").format("MMMM");
 }
 
 export function getIsoWeekDaysInMonth(year, month) {
@@ -56,14 +44,29 @@ export function getIsoWeekDaysInMonth(year, month) {
   return d.asDays() + 1;
 }
 
+export function getAppointments() {
+  return [
+    {
+      id: 1,
+      date: new Date("2020-03-12 18:00"),
+      description: "Tech Event Dinner!",
+      important: true
+    },
+    {
+      id: 2,
+      date: new Date("2020-03-12 19:00"),
+      description: "Workshops Session I",
+      important: false
+    },
+    {
+      id: 3,
+      date: new Date("2020-03-12 20:00"),
+      description: "Workshops Session II",
+      important: false
+    }
+  ];
+}
+
 moment.prototype.monthId = function() {
   return Number(this.format("YYYYMM"));
-};
-
-moment.prototype.weekId = function() {
-  return Number(this.format("YYYYWW"));
-};
-
-moment.prototype.dayId = function() {
-  return Number(this.format("YYYYMMDD"));
 };
